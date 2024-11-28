1ccd3a180b0f5dcab4c2df7e1b1b116620e1e86b
John <muller.john@gmail.com>
2024-11-28 09:41:52 +0100

    remove some useless mocks in GitHubProcessorTests

--- tests/GitHubTreeProcessorTests.cs
+++ tests/GitHubTreeProcessorTests.cs
@@ -10,37 +10,25 @@ public class GitHubTreeProcessorTests
     public async Task ProcessTree_WithFilters_OnlyProcessesMatchingItems()
     {
         var mockClient = new Mock<IGitClient>();
-        var mockFilter = new Mock<IGitHubDataFilter>();
-        var mockFilter2 = new Mock<IGitHubDataFilter>();
         var statistics = new StatisticsCollector();
 
         var tree = new GitHubTree
         {
             Tree = new List<GitHubTreeItem>
             {
-                new GitHubTreeItem { Path = "test1.js", Url = "url1", Type = "blob" },
+                new GitHubTreeItem { Path = "lib/test1.js", Url = "url1", Type = "blob" },
                 new GitHubTreeItem { Path = "test2.txt", Url = "url2", Type = "blob" },
-                new GitHubTreeItem { Path = "test3.js", Url = "url3", Type = "blob" },
+                new GitHubTreeItem { Path = "lib/test3.js", Url = "url3", Type = "blob" },
                 new GitHubTreeItem { Path = ".git", Url = "plop", Type = "tree" }
             }
         };
-
-        mockFilter.Setup(f => f.Accept(It.Is<GitHubTreeItem>(i => i.Path.EndsWith(".js"))))
-            .Returns(true);
-        mockFilter.Setup(f => f.Accept(It.Is<GitHubTreeItem>(i => !i.Path.EndsWith(".js"))))
-            .Returns(false);
-        
-        mockFilter2.Setup(f => f.Accept(It.Is<GitHubTreeItem>(i => i.Type == "blob")))
-            .Returns(true);
-        mockFilter2.Setup(f => f.Accept(It.Is<GitHubTreeItem>(i => i.Type == "tree")))
-            .Returns(false);
 
         mockClient.Setup(c => c.GetBlobContent("url1"))
             .ReturnsAsync("content1");
         mockClient.Setup(c => c.GetBlobContent("url3"))
             .ReturnsAsync("content3");
 
-        var processor = new GitHubTreeProcessor(mockClient.Object, new List<IGitHubDataFilter> { mockFilter.Object, mockFilter2.Object });
+        var processor = new GitHubTreeProcessor(mockClient.Object, new List<IGitHubDataFilter> { new TreeItemFilter(), new JsFileFilter() });
 
         await processor.ProcessTree(tree, statistics);
 
